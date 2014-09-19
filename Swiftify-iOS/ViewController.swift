d16f5b6c6accecae3614aae124307cbb2559d278
Peter Marton <peter@secretsaucepartners.com>
2014-09-19 13:36:30 +0200

    Initial Commit

---
+++ Swiftify-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftify-iOS
+//
+//  Created by Peter Marton on 19/09/14.
+//  Copyright (c) 2014 Peter Marton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
