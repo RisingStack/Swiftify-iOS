019a56b45387d458f94bbb303afc544413796a1f
Amos Elmaliah <amosel@gmail.com>
2015-03-09 11:57:42 +0200

    Update view controller to new swift API.

--- Swiftify-iOS/ViewController.swift
+++ Swiftify-iOS/ViewController.swift
@@ -25,77 +25,85 @@ class ViewController: UIViewController {
     var views: Dictionary<String, AnyObject> = [:]
     var dataTemp: Array<CGFloat> = []
     var dataPWM: Array<CGFloat> = []
-    
+
+	func newLineChartView() -> LineChart {
+		let view = LineChart()
+		view.areaUnderLinesVisible = true
+		view.labelsYVisible = true
+		view.axisInset = 40
+		view.setTranslatesAutoresizingMaskIntoConstraints(false)
+		return view;
+	}
+
+	func evaluateBrwosifiedJSString(jsSource :String) -> Void {
+
+		var context = JSContext()
+
+		context.evaluateScript(jsSource)
+
+		let calculate = context.objectForKeyedSubscript("calculate")
+		let JSON = context.objectForKeyedSubscript("JSON")
+		let JSONparse = JSON.objectForKeyedSubscript("parse")
+		let JSONString = "{\"foo\":\"bar\",\"bar\":[1,2],\"world\":{\"hello\":4,\"foo\":\"bar\"}}"
+
+		let parsed = JSONparse.callWithArguments([JSONString])
+		let parsedDic = parsed.toDictionary()
+
+		// Simulate temp
+		let setPoint = context.objectForKeyedSubscript("setPoint")
+		setPoint.callWithArguments([REF_TEMP])
+
+		var temp = MIN_TEMP
+		var amplitude = 0.3;       // wave amplitude
+		var freq = 1.0;            // angular frequency
+
+
+		var dataTemp: Array<CGFloat> = []
+		var dataPWM: Array<CGFloat> = []
+		for var index = -1.0; index < 1.0; index += 0.1 {
+			let deltaTemp = sin(freq * (index)) * amplitude;
+
+			temp -= deltaTemp
+
+			let output = calculate.callWithArguments([temp]).toDouble()
+
+			dataTemp.append(CGFloat(temp));
+			dataPWM.append(CGFloat(output / 10));
+		}
+
+		self.dataPWM = dataPWM;
+		self.dataTemp = dataTemp;
+	}
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // load javascript file in String
-        let path = NSBundle.mainBundle().pathForResource("bundle", ofType: "js")
-        var jsSource: String! = String.stringWithContentsOfFile(path!)
-        
-        // Make browserify to work
-        jsSource = "var window = this; \(jsSource)"
-        
-        // create a javascript context environment and evaluate script
-        var context = JSContext()
-        
-        context.evaluateScript(jsSource)
-        
-        
-        // JSON parsing
-        let calculate = context.objectForKeyedSubscript("calculate")
-        let JSON = context.objectForKeyedSubscript("JSON")
-        let JSONparse = JSON.objectForKeyedSubscript("parse")
-        let JSONString = "{\"foo\":\"bar\",\"bar\":[1,2],\"world\":{\"hello\":4,\"foo\":\"bar\"}}"
-        
-        let parsed = JSONparse.callWithArguments([JSONString])
-        let parsedDic = parsed.toDictionary()
-        
-        // Simulate temp
-        let setPoint = context.objectForKeyedSubscript("setPoint")
-        setPoint.callWithArguments([REF_TEMP])
-        
-        var temp = MIN_TEMP
-        var amplitude = 0.3;       // wave amplitude
-        var freq = 1.0;            // angular frequency
-        
-        for var index = -1.0; index < 1.0; index += 0.1 {
-            let deltaTemp = sin(freq * (index)) * amplitude;
-            
-            temp -= deltaTemp
-            
-            let output = calculate.callWithArguments([temp]).toNumber()
-            
-            self.dataTemp.append(CGFloat(temp));
-            self.dataPWM.append((output / 10));
-        }
-        
-        // Temp chart
-        lineChartTemp = LineChart()
-        lineChartTemp!.areaUnderLinesVisible = true
-        lineChartTemp!.labelsYVisible = true
-        lineChartTemp!.axisInset = 40
-        lineChartTemp!.addLine(dataTemp)
-        lineChartTemp!.setTranslatesAutoresizingMaskIntoConstraints(false)
-        views["lineChartTemp"] = lineChartTemp
-        tempView.addSubview(lineChartTemp!)
-        
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[lineChartTemp]-|", options: nil, metrics: nil, views: views))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[lineChartTemp]-|", options: nil, metrics: nil, views: views))
-        
+		if let path = NSBundle.mainBundle().pathForResource("bundle", ofType: "js") {
+			var jsSource : String = String(contentsOfFile:path)!
+			jsSource = "var window = this; \(jsSource)"
+			self.evaluateBrwosifiedJSString(jsSource)
+		}
+
+		// Temp chart
+		let lineChartTemp = self.newLineChartView()
+		lineChartTemp.addLine(self.dataTemp)
+		views["lineChartTemp"] = lineChartTemp
+		tempView.addSubview(lineChartTemp)
+		view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[lineChartTemp]-|", options: nil, metrics: nil, views: views))
+		view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[lineChartTemp]-|", options: nil, metrics: nil, views: views))
+
         // PWM chart
-        lineChartPWM = LineChart()
-        lineChartPWM!.areaUnderLinesVisible = true
-        lineChartPWM!.labelsYVisible = true
-        lineChartPWM!.axisInset = 40
-        lineChartPWM!.addLine(dataPWM)
-        lineChartPWM!.setTranslatesAutoresizingMaskIntoConstraints(false)
+        let lineChartPWM = self.newLineChartView()
+        lineChartPWM.addLine(self.dataPWM)
         views["lineChartPWM"] = lineChartPWM
-        pwmView.addSubview(lineChartPWM!)
+        pwmView.addSubview(lineChartPWM)
         
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[lineChartPWM]-|", options: nil, metrics: nil, views: views))
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[lineChartPWM]-|", options: nil, metrics: nil, views: views))
-        
+
+		self.lineChartPWM = lineChartPWM;
+		self.lineChartTemp = lineChartTemp;
     }
     
     
